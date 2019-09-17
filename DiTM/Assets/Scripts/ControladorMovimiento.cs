67b292437f0a21451d4c1ef2a4f3add0b61facc8
Raúl <albieramirez88@gmail.com>
2019-09-16 23:10:37 -0600

    Movimiento
    Movimiento de protagonista completo
    Sprites adicionales agregados

--- DiTM/Assets/Scripts/ControladorMovimiento.cs
+++ DiTM/Assets/Scripts/ControladorMovimiento.cs
@@ -4,42 +4,79 @@ using UnityEngine;
 
 public class ControladorMovimiento : MonoBehaviour
 {
-    public float moveSpeed=5f;
-    public Animator animator;
+  
+  public float speed;
+  public float moveSpeed = 5f;
+  private Rigidbody2D rb;
+  private Animator anim;
+  public bool isGrounded=false;
+
 
     // Start is called before the first frame update
 
     void Start()
     {
-
+        anim= GetComponent<Animator>();
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mvmnt = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position+= mvmnt* Time.deltaTime * moveSpeed;
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            animator.SetBool("Moviendose", true);
-            transform.localScale = new Vector3(0.2246417F,0.2246417F,0.2246417F);
-        }
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
-        {
-            animator.SetBool("Moviendose", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            animator.SetBool("Moviendose", true);
-            transform.localScale = new Vector3(-0.2246417F,0.2246417F,0.2246417F);
-
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            animator.SetBool("Moviendose", false);
-        }
-
+        Jump();
+        Attack();
+        float moveInput= Input.GetAxisRaw("Horizontal");
+        rb.velocity=new Vector2(moveInput*speed, rb.velocity.y);
+        Vector3 movement= new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position+= movement*Time.deltaTime*moveSpeed;
         
+        if (moveInput==0)
+        {
+            anim.SetBool("Moviendose",false);
+        }
+        else
+        {
+            anim.SetBool("Moviendose",true);
+
+        }
+
+        if (moveInput<0)
+        {
+            transform.eulerAngles=new Vector3(0,0,0);
+        }
+        else if (moveInput>0)
+        {
+            transform.eulerAngles= new Vector3(0,180,0);   
+        }
+
+        void Jump()
+        {
+            if (Input.GetButtonDown("Jump") && isGrounded==true)
+            {
+                anim.SetBool("Saltando",true);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,5f), ForceMode2D.Impulse);
+                StartCoroutine(stopJump());
+            }
+            
+        }
+
+        IEnumerator stopJump()
+        {
+            yield return new WaitForSecondsRealtime(1);
+            anim.SetBool("Saltando",false);
+        }
+
+
+        void Attack()
+        {
+            if(Input.GetKeyDown("z"))
+            {
+                anim.SetBool("Atacando",true);
+            }
+            if (Input.GetKeyUp("z"))
+            {
+                anim.SetBool("Atacando",false);
+            }
+        }
     }
 }
